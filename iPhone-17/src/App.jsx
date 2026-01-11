6aa284a476d87c6a1ef5fba1351b7838dad48afa
Nathan-VE <nvictoresposito@gmail.com>
2026-01-11 16:56:32 -0300

    feat: initial project structure

---
+++ iPhone-17/src/App.jsx
@@ -0,0 +1,21 @@
+import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
+import Intro from "./components/Intro.jsx";
+import Highlights from "./components/Highlights.jsx";
+import Colors from "./components/Colors.jsx";
+import Footer from "./components/Footer.jsx";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+      <Intro />
+      <Highlights />
+      <Colors />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
